0fdf0622c855800c7e7896b81a1a68adbec4f1a6
Marcel Widmer <marzelwidmer@gmail.com>
2018-06-17 16:39:42 +0200

    inital commit

---
+++ bar-service/src/main/java/ch/keepcalm/cloud/service/bar/BarServiceApplication.java
@@ -0,0 +1,23 @@
+package ch.keepcalm.cloud.service.bar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BarServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarServiceApplication.class, args);
+    }
+}
+
+@RestController
+class BarController {
+
+    @GetMapping("/")
+    String bar() {
+        return "bar";
+    }
+}
