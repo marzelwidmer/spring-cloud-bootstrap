d7e200615a0f28e32333a7d064e5fad538a816a1
Marcel Widmer <marzelwidmer@gmail.com>
2018-06-17 18:42:19 +0200

    add unsecure profile

--- bar-service/src/main/java/ch/keepcalm/cloud/service/bar/BarServiceApplication.java
+++ bar-service/src/main/java/ch/keepcalm/cloud/service/bar/BarServiceApplication.java
@@ -2,6 +2,13 @@ package ch.keepcalm.cloud.service.bar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +18,28 @@ public class BarServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BarServiceApplication.class, args);
     }
+
+
+    @Configuration
+    @Profile("unsecure")
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // https://stackoverflow.com/questions/48902706/spring-cloud-eureka-with-spring-security/
+            http.csrf().disable()
+                    .authorizeRequests()
+                    .anyRequest().permitAll();
+        }
+    }
 }
 
 @RestController
 class BarController {
 
     @GetMapping("/")
-    String bar() {
-        return "bar";
+    public ResponseEntity<String> bar() {
+        return new ResponseEntity<>("bar ", HttpStatus.OK);
     }
+
 }
--- foo-service/src/main/java/ch/keepcalm/cloud/service/foo/FooServiceApplication.java
+++ foo-service/src/main/java/ch/keepcalm/cloud/service/foo/FooServiceApplication.java
@@ -6,13 +6,21 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient // need to reach the bar-service, otherwise "java.lang.IllegalStateException: No instances available for bar-service"
+@EnableDiscoveryClient
+// need to reach the bar-service, otherwise "java.lang.IllegalStateException: No instances available for bar-service"
 public class FooServiceApplication {
 
     @LoadBalanced
@@ -24,6 +32,21 @@ public class FooServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FooServiceApplication.class, args);
     }
+
+
+    @Configuration
+    @Profile("unsecure")
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // https://stackoverflow.com/questions/48902706/spring-cloud-eureka-with-spring-security/
+            http.csrf().disable()
+                    .authorizeRequests()
+                    .anyRequest().permitAll();
+        }
+    }
+
 }
 
 @RestController
@@ -35,15 +58,18 @@ class FooController {
         this.barClient = barClient;
     }
 
-    @GetMapping("/")
-    public String foo() {
-        return "foo";
+    @GetMapping("/foobar")
+    public ResponseEntity<String> fooBar() {
+        return new ResponseEntity<>("foo " + barClient.getBar(), HttpStatus.OK);
     }
 
-    @GetMapping("/foobar")
-    public String fooBar() {
-        return "foo" + barClient.getBar();
+
+
+    @GetMapping("/")
+    public ResponseEntity<String> foo() {
+        return new ResponseEntity<>("foo ", HttpStatus.OK);
     }
+
 
 }
 
