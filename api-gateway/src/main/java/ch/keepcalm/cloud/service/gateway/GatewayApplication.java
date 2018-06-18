f6dc9f4e85896aba54062a6debc6feb946dc0ff4
Marcel Widmer <marzelwidmer@gmail.com>
2018-06-18 09:32:13 +0200

    add api gateway

---
+++ api-gateway/src/main/java/ch/keepcalm/cloud/service/gateway/GatewayApplication.java
@@ -0,0 +1,30 @@
+package ch.keepcalm.cloud.service.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Configuration
+    @Profile("unsecure")
+    @EnableWebFluxSecurity
+    class WebSecurityConfig {
+        @Bean
+        public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+            http
+            .csrf().disable().authorizeExchange().anyExchange().permitAll();
+            return http.build();
+        }
+    }
+}
