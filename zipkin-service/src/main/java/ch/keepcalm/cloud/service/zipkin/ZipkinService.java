811548521734a2cfa28a159b82a33007ff661fbb
Marcel Widmer <marzelwidmer@gmail.com>
2018-06-23 16:32:03 +0200

    clean Up configuration setup

--- zipkin-service/src/main/java/ch/keepcalm/cloud/service/zipkin/ZipkinService.java
+++ zipkin-service/src/main/java/ch/keepcalm/cloud/service/zipkin/ZipkinService.java
@@ -3,6 +3,11 @@ package ch.keepcalm.cloud.service.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import zipkin.server.internal.EnableZipkinServer;
 
 
@@ -10,7 +15,22 @@ import zipkin.server.internal.EnableZipkinServer;
 @EnableEurekaClient
 @EnableZipkinServer
 public class ZipkinService {
+
     public static void main(String[] args) {
         SpringApplication.run(ZipkinService.class, args);
     }
+
+    @Configuration
+    @Profile("unsecure")
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // https://stackoverflow.com/questions/48902706/spring-cloud-eureka-with-spring-security/
+            http.csrf().disable()
+                    .authorizeRequests()
+                    .anyRequest().permitAll();
+        }
+    }
+
 }
