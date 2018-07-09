b3b4b88090a5bd57e37a17652c8d6027c70404d5
Marcel Widmer <marzelwidmer@gmail.com>
2018-07-09 19:44:50 +0200

    chagne contract

--- nutrition-service/src/test/resources/contracts/find_Agavensirup.groovy
+++ nutrition-service/src/test/resources/contracts/find_Agavensirup.groovy
@@ -16,37 +16,39 @@ Contract.make {
         }
         status 200
         body(
-                _links: [
-                        self: [
-                                href: value(
-                                        consumer("http://${fromRequest().header('Host')}${fromRequest().url()}"),
-                                        producer("http://localhost${fromRequest().path()}")
-                                )
-                        ],
-                        'find' : [
-                                href: value(
-                                        consumer("http://${fromRequest().header('Host')}/foods/{name}"),
-                                        producer("http://localhost/foods{?name}")
-                                ),
-                                templated: true
-                        ]
+            _links: [
+                self  : [
+                    href: value(
+                        consumer("http://${fromRequest().header('Host')}${fromRequest().url()}"),
+                        producer("http://localhost${fromRequest().path()}")
+                    )
                 ],
-                _embedded: [
-                    foods : [
-                        name: "Agavensirup",
-//                        _links:
-//                                [
-//                                self:
-//                        [
-//                                        href: value(
-//                                                consumer("http://${fromRequest().header('Host')}${fromRequest().url()}"),
-//                                                producer("http://localhost${fromRequest().path()}/5b3877a712f6466db803da0e")
-//                                        )
-//                                ]
-//                        ]
+                'find': [
+                    href     : value(
+                        consumer("http://${fromRequest().header('Host')}/foods/{name}"),
+                        producer("http://localhost/foods{?name}")
+                    ),
+                    templated: true
+                ]
+            ],
+            _embedded: [
+                foods: [
+                    [
+                        name  : "Agavensirup",
+                        _links:
+                            [
+                                self:
+                                    [
+                                        href: value(
+                                            consumer("http://${fromRequest().header('Host')}${fromRequest().url()}"),
+                                            producer("http://localhost${fromRequest().path()}/5b3877a712f6466db803da0e")
+                                        )
+                                    ]
+                            ]
                     ]
-                ],
-                totalItems : 1
+                ]
+            ],
+            totalItems: 1
         )
     }
 }
