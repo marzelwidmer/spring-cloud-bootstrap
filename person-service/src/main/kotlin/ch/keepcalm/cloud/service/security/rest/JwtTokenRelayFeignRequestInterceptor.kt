dc68af54573b8f663720ac13b9bc6c42ebac9253
Marcel Widmer <marzelwidmer@gmail.com>
2018-07-29 08:40:35 +0200

    add lates security stuff

--- person-service/src/main/kotlin/ch/keepcalm/cloud/service/security/rest/JwtTokenRelayFeignRequestInterceptor.kt
+++ person-service/src/main/kotlin/ch/keepcalm/cloud/service/security/rest/JwtTokenRelayFeignRequestInterceptor.kt
@@ -2,13 +2,15 @@ package ch.keepcalm.cloud.service.security.rest
 
 import feign.RequestInterceptor
 import feign.RequestTemplate
-import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.security.core.context.SecurityContextHolder
 
 class JwtTokenRelayFeignRequestInterceptor : RequestInterceptor {
     override fun apply(template: RequestTemplate) {
-        SecurityContextHolder.getContext().authentication?.let {
-            template.header(HttpHeaders.AUTHORIZATION, "Bearer $it.credentials")
+        if (!template.headers().containsKey(AUTHORIZATION)) {
+            SecurityContextHolder.getContext().authentication?.let {
+                template.header(AUTHORIZATION, "Bearer ${it.credentials}")
+            }
         }
     }
 }
--- person-service/src/main/kotlin/ch/keepcalm/cloud/service/security/rest/JwtTokenRelayHttpRequestInterceptor.kt
+++ person-service/src/main/kotlin/ch/keepcalm/cloud/service/security/rest/JwtTokenRelayHttpRequestInterceptor.kt
@@ -1,6 +1,6 @@
 package ch.keepcalm.cloud.service.security.rest
 
-import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.http.HttpRequest
 import org.springframework.http.client.ClientHttpRequestExecution
 import org.springframework.http.client.ClientHttpRequestInterceptor
@@ -9,8 +9,10 @@ import org.springframework.security.core.context.SecurityContextHolder
 
 class JwtTokenRelayHttpRequestInterceptor : ClientHttpRequestInterceptor {
     override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution): ClientHttpResponse {
-        SecurityContextHolder.getContext().authentication?.let {
-            request.headers.add(HttpHeaders.AUTHORIZATION, "Bearer $it.credentials")
+        if (!request.headers.containsKey(AUTHORIZATION)) {
+            SecurityContextHolder.getContext().authentication?.let {
+                request.headers.set(AUTHORIZATION, "Bearer ${it.credentials}")
+            }
         }
         return execution.execute(request, body)
     }
